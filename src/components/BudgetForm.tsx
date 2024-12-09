510e145f16b484f36b90501d6c0b06586b74dddb
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-09 18:00:50 -0300

    Creando el formulario para definir el presupuesto

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,5 +1,35 @@
+import {useState, ChangeEvent} from "react"
+
 export default function BudgetForm() {
+    const [budget, setBudget] = useState(0)
+    const handleChange = (e : ChangeEvent<HTMLInputElement>) => {
+        setBudget(e.target.valueAsNumber)
+
+        
+    }
     return (
-        <div>BudgetForm</div>
+        <form className="space-y-5">
+            <div className="flex flex-col space-y-5">
+                <label htmlFor="budget" className="text-4xl text-blue-600 font-bold text-center">
+                    Definir Presupuesto
+                </label>
+
+                <input  
+                    id="budget"
+                    type="number"
+                    className="w-full bg-white border boder-gray-200 p-2"
+                    placeholder="Define tu presupuesto"
+                    name="budget"
+                    value={budget}
+                    onChange={handleChange}
+                />
+            </div>
+
+            <input
+                type="submit"
+                value='Definir Presupuesto'
+                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+            />
+        </form>
     )
 }
