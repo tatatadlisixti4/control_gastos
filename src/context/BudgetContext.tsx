8702e48e678e1d8bb1012dceab1768b07ff03193
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-09 21:27:31 -0300

    Definiendo el context

---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,29 @@
+// Importaciones
+import {useReducer, createContext, Dispatch, ReactNode} from "react"
+import {BudgetActions, BudgetState, budgetReducer, initialState} from "../reducers/budget-reducer"
+
+// Types Props
+type BudgetContextProps = {
+    state: BudgetState
+    dispatch: Dispatch<BudgetActions>
+}
+
+type BudgetProviderProps = {
+    children: ReactNode
+}
+
+// Definición de contexto: 
+export const BudgetContext = createContext<BudgetContextProps>({} as BudgetContextProps)
+
+// Definición de proveedor
+export const BudgetProvider = ({children} : BudgetProviderProps) => {
+    const [state, dispatch] = useReducer(budgetReducer, initialState)
+    return (
+        <BudgetContext.Provider
+            value={{
+                state,
+                dispatch
+            }}
+        > {children} </BudgetContext.Provider>
+    )
+}   
