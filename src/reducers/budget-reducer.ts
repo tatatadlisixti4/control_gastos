7182ce7b27aae1e8e40db1d279ed43e31ae135ed
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-10 16:40:30 -0300

    Mostrando el modal agregando un nuevo gasto

--- src/reducers/budget-reducer.ts
+++ src/reducers/budget-reducer.ts
@@ -1,12 +1,15 @@
 export type BudgetActions =
-    {type: 'add-budget', payload: {budget: number}}
+    {type: 'add-budget', payload: {budget: number}} |
+    {type: 'show-modal'}
 
 export type BudgetState = {
     budget: number
+    modal: boolean
 }
 
 export const initialState: BudgetState = {
-    budget: 0
+    budget: 0,
+    modal: false
 }
 
 export const budgetReducer = (
@@ -17,6 +20,12 @@ export const budgetReducer = (
         return {
             ...state,
             budget: action.payload.budget
+        }
+    }
+    if(action.type === 'show-modal') {
+        return {
+            ...state,
+            modal: true
         }
     }
     return state
