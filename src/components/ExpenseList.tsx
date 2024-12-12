6627e7077a69d674ef0ccc40fbf890b7788db27e
Thadli Guerra <tg.ibarra64@gmail.com>
2024-12-12 20:19:01 -0300

    Mostrar los gastos de la categoría seleccionada y building de la app

--- src/components/ExpenseList.tsx
+++ src/components/ExpenseList.tsx
@@ -3,13 +3,15 @@ import {useBugdet} from "../hooks/useBudget"
 import ExpenseDetailt from "./ExpenseDetailt"
 export default function ExpenseList() {
     const {state} = useBugdet() 
-    const isEmpty = useMemo(() => state.expenses.length === 0, [state.expenses])
+    const filteredExpenses = state.currentCategory? state.expenses.filter(expense => expense.category ===  state.currentCategory): state.expenses
+    const isEmpty = useMemo(() => filteredExpenses.length === 0, [filteredExpenses])
+    
     return (
         <div className="mt-10 bg-white shadow-lg rounded-lg p-10">
             {isEmpty ? <p className="text-gray-600 text-2xl font-bold">No Hay Gastos:</p> : (
                 <>
                     <p className="text-gray-600 text-2xl font-bold my-5">Listado de Gastos</p>
-                    {state.expenses.map(expense => (
+                    {filteredExpenses.map(expense => (
                         <ExpenseDetailt
                             key={expense.id}
                             expense={expense} 
